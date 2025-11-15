732ccb30e84722051296ea7b7457186725018e28
Dheeraj R Shrivatsa <dheerajshrivatsa@gmail.com>
2025-11-15 16:37:41 +0530

    Intital rebrand to Project Flare

--- config/common_mobile_full.mk
+++ config/common_mobile_full.mk
@@ -1,29 +1,11 @@
-# Inherit common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_mobile.mk)
+# Inherit common ProjectFlare stuff
+$(call inherit-product, vendor/flare/config/common_mobile.mk)
 
 PRODUCT_SIZE := full
-
-# Include {Lato,Rubik} fonts
-$(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
-$(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
-
-# Apps
-PRODUCT_PACKAGES += \
-    Camelot \
-    Etar \
-    Profiles \
-    Recorder \
-    Seedvault \
-    Twelve
 
 ifneq ($(PRODUCT_NO_CAMERA),true)
 PRODUCT_PACKAGES += \
     Aperture
-endif
-
-ifneq ($(TARGET_EXCLUDES_AUDIOFX),true)
-PRODUCT_PACKAGES += \
-    AudioFX
 endif
 
 # Extra cmdline tools
@@ -31,12 +13,7 @@ PRODUCT_PACKAGES += \
     unrar \
     zstd
 
-# Fonts
-PRODUCT_PACKAGES += \
-    fonts_customization.xml \
-    FontLatoOverlay \
-    FontRubikOverlay
 
-# Include Lineage LatinIME dictionaries
-PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/dictionaries
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/lineage/overlay/dictionaries
+# Include flare LatinIME dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/flare/overlay/dictionaries
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/flare/overlay/dictionaries
